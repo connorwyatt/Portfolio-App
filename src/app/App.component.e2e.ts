02231dcd633fa40074793aa89d3cc189a0c7f9c0
Connor Wyatt <connorwyatt1@gmail.com>
2016-10-08 14:46:36 +0100

    Added e2e test for when route is not defined

--- src/app/App.component.e2e.ts
+++ src/app/App.component.e2e.ts
@@ -9,4 +9,17 @@ describe('App', () => {
       done();
     });
   });
+
+  describe('when navigating to a URL that has no route defined', () => {
+    beforeEach(() => {
+      browser.get('/no-route-defined-for-this-url');
+    });
+
+    it('should redirect to `not-found`', function(done) {
+      browser.getCurrentUrl().then((currentUrl: string) => {
+        expect(currentUrl.endsWith('/not-found')).toBe(true);
+        done();
+      });
+    });
+  });
 });
