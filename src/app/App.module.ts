0d20f7609535d644d1332efd44286da5fb0eb62f
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-10-14 17:39:20 +0100

    Removed Angular Router routes and injection

--- src/app/App.module.ts
+++ src/app/App.module.ts
@@ -1,31 +1,20 @@
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
 import {AppComponent} from './App.component';
 import {ENTITIES} from './constants/ENTITIES.constant';
 import {TITLES} from './constants/TITLES.constant';
-import {GALLERIES_ROUTE} from './routes/galleries/Galleries.route';
-import {GalleriesResolveService} from './routes/galleries/GalleriesResolve.service';
-import {NOT_FOUND_ROUTE} from './routes/notFound/NotFound.route';
 import {APIService} from './services/API.service';
 import {ModelService} from './services/Model.service';
-import {TitleResolveService} from './services/resolves/TitleResolve.service';
 import {ENTITIES_TOKEN} from './tokens/ENTITIES.token';
 import {TITLES_TOKEN} from './tokens/TITLES.token';
 
 @NgModule({
-  imports: [
-    BrowserModule, HttpModule, RouterModule.forRoot([
-      GALLERIES_ROUTE, NOT_FOUND_ROUTE,
-      {path: '', redirectTo: GALLERIES_ROUTE.path, pathMatch: 'full'},
-      {path: '**', redirectTo: NOT_FOUND_ROUTE.path, pathMatch: 'full'}
-    ])
-  ],
-  declarations: [GALLERIES_ROUTE.component, NOT_FOUND_ROUTE.component, AppComponent],
+  imports: [BrowserModule, HttpModule],
+  declarations: [AppComponent],
   providers: [
-    TitleResolveService, GalleriesResolveService, APIService, ModelService,
-    {provide: TITLES_TOKEN, useValue: TITLES}, {provide: ENTITIES_TOKEN, useValue: ENTITIES}
+    APIService, ModelService, {provide: TITLES_TOKEN, useValue: TITLES},
+    {provide: ENTITIES_TOKEN, useValue: ENTITIES}
   ],
   bootstrap: [AppComponent]
 })
