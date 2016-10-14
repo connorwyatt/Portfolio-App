2bfbdbe7d2f0b78b796d7090cf9d960d9ae4c413
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-10-14 20:42:20 +0100

    Added resolve for galleries state

--- src/app/states/galleries/Galleries.states.ts
+++ src/app/states/galleries/Galleries.states.ts
@@ -1,11 +1,20 @@
+import 'rxjs/add/operator/toPromise';
 import {Ng2StateDeclaration} from 'ui-router-ng2';
+import {APIService} from '../../services/API.service';
 import {GalleriesComponent} from './Galleries.component';
 
 export const GalleriesState: Ng2StateDeclaration = {
   name: 'galleries',
   parent: 'app',
   url: '/galleries',
-  component: GalleriesComponent
+  component: GalleriesComponent,
+  resolve: [{
+    token: 'galleries',
+    deps: [APIService],
+    resolveFn: (apiService: APIService) => {
+      return apiService.getCollection('/api/galleries').toPromise();
+    }
+  }]
 };
 
 export const GalleriesStates: Array<Ng2StateDeclaration> = [GalleriesState];
