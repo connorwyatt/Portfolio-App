46dc9649b6f078af8ec5a9d6a8038a0ef2b0879d
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-10-14 18:27:30 +0100

    Removed Angular Router resolve data from GalleriesComponent

--- src/app/states/galleries/Galleries.component.ts
+++ src/app/states/galleries/Galleries.component.ts
@@ -1,22 +1,10 @@
 import 'rxjs/add/operator/take';
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Gallery} from '../../entities/Gallery.entity';
 
 @Component({
   template: require('./Galleries.component.html'),
   styles: [require('./Galleries.component.scss')]
 })
 export class GalleriesComponent {
-  get galleries() {
-    return this._galleries;
-  };
-
-  private _galleries: Gallery[];
-
-  constructor(route: ActivatedRoute) {
-    route.data.take(1).subscribe((data: {galleries: Gallery[]}) => {
-      this._galleries = data.galleries;
-    });
-  }
+  constructor() {}
 }
