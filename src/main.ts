d511895139d1cd6c9a42414e8ed05eb6299dc7e7
Connor Wyatt <connorwyatt1@gmail.com>
2016-10-27 22:43:17 +0100

    Removed hot module reloading and unused packages

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/App.module';
 
-export function main(): Promise<any> {
-  return platformBrowserDynamic().bootstrapModule(AppModule);
-}
-
 if (ENV === 'PRODUCTION') {
   enableProdMode();
-  main();
-} else {
-  let angular2hmr = require('angular2-hmr');
-  angular2hmr.hotModuleReplacement(main, module);
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
