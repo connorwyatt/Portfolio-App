7cf75cd3a4e3d6691d8c4497948bd00a7567444c
Connor Wyatt <connorwyatt1@gmail.com>
2016-10-27 19:44:44 +0100

    Added header component to header view

--- src/app/states/galleries/Galleries.states.ts
+++ src/app/states/galleries/Galleries.states.ts
@@ -2,6 +2,7 @@ import 'rxjs/add/operator/toPromise';
 import {Ng2StateDeclaration} from 'ui-router-ng2';
 import {Gallery} from '../../entities';
 import {APIService} from '../../services/API.service';
+import {HeaderComponent} from '../header/Header.component';
 import {GalleriesComponent} from './Galleries.component';
 import {GalleryStates} from './gallery/Gallery.states';
 
@@ -9,7 +10,7 @@ export const GalleriesState: Ng2StateDeclaration = {
   name: 'galleries',
   parent: 'app',
   url: '/galleries',
-  views: {'main@app': {component: GalleriesComponent}},
+  views: {'header@app': {component: HeaderComponent}, 'main@app': {component: GalleriesComponent}},
   resolve: [{
     token: 'galleries',
     deps: [APIService],
