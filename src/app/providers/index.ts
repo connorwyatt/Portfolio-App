08cf6a4bd4021a0901ab5f00c21d11f6ca607864
Connor Wyatt <connorwyatt1@gmail.com>
2016-10-28 18:31:02 +0100

    Optimised provider imports

---
+++ src/app/providers/index.ts
@@ -0,0 +1,7 @@
+export * from './CONSOLE.provider';
+export * from './ELEMENT_RESIZE_DETECTOR.provider';
+export * from './ENTITIES_CONSTANT.provider';
+export * from './ENV_SERVICE.provider';
+export * from './FILE_READER.provider';
+export * from './REFLECTIVE_INJECTOR.provider';
+export * from './TITLES_CONSTANT.provider';
